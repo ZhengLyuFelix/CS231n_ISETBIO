58d8cc3768292b08f689b0bb5a0228a1be51a53c
ZhengLyuFelix <33340945+ZhengLyuFelix@users.noreply.github.com>
2018-05-13 12:23:26 -0700

    Add files via upload

---
+++ PossionMLE.m
@@ -0,0 +1,20 @@
+% The function to calculate Poisson MLE.
+% The input will be the coneMosaic absorption
+% Inputs: 
+%   timeInterval  - duration of each time step
+%   absorption    - the coneMosaic absorption used to be classified
+%   meanIsorate   - the mean isomerization rate (distribution) for each frame(time step)
+%                   for the whole cones (w/ and w/o stimulus)
+
+%% Initialization (temporary)
+
+timeInterval = 0.005; %(5ms)
+
+
+%% Load the absorption data (to be passed in the future)
+
+%% Calculate the probability given there's no stimulus
+
+%% Calculate the probability given there's stimulus
+
+%% Compare the MLE to determine if there's stimulus or not
